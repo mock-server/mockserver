4cbc5389ccbd565d5a304b64a462ef423fc43f92
James Bloom <jamesdbloom@gmail.com>
2013-05-01 19:58:44 +0100

    improved logging and added easy support to run from gradle using 'gradle run -Pport=8999'

--- src/main/java/org/mockserver/server/EmbeddedJettyRunner.java
+++ src/main/java/org/mockserver/server/EmbeddedJettyRunner.java
@@ -9,13 +9,14 @@ import org.slf4j.LoggerFactory;
  * @author jamesdbloom
  */
 public class EmbeddedJettyRunner {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static Logger logger = LoggerFactory.getLogger(EmbeddedJettyRunner.class);
 
     private final Server server;
 
     public static void main(String[] args) throws Exception {
         int port = args.length == 1 ? Integer.parseInt(args[0]) : 8080;
         new EmbeddedJettyRunner(port);
+        logger.info("Started mock server listening on " + port);
     }
 
     public EmbeddedJettyRunner(int port) {
