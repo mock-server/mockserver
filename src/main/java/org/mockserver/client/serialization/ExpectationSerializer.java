5c3a0313e5c71cf0f70a16aa904c6ee7f5d65daa
James D Bloom <jamesdbloom@gmail.com>
2013-05-09 11:00:48 +0100

    fixing character encoding for logging

--- src/main/java/org/mockserver/client/serialization/ExpectationSerializer.java
+++ src/main/java/org/mockserver/client/serialization/ExpectationSerializer.java
@@ -35,7 +35,7 @@ public class ExpectationSerializer {
         Expectation expectation = null;
         try {
             byte[] jsonExpectation = IOUtils.toByteArray(new InputStreamReader(inputStream), Charset.forName(CharEncoding.UTF_8));
-            logger.debug("Received JSON expectation:\n" + new String(jsonExpectation));
+            logger.debug("Received JSON expectation:\n" + new String(jsonExpectation, Charset.forName(CharEncoding.UTF_8)));
             ExpectationDTO expectationDTO = objectMapper.readValue(jsonExpectation, ExpectationDTO.class);
             if (expectationDTO != null) {
                 expectation = expectationDTO.buildObject();
