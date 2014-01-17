50525061e95107064a68c751cb32ee3ec5aeb8b3
James Bloom <jamesdbloom@gmail.com>
2014-01-17 19:47:34 +0000

    fixed multiple minor issues and fixed examples to make sure it deploys properly

--- mockserver-maven-plugin/src/test/java/org/mockserver/maven/MockServerRunAndWaitMojoTest.java
+++ mockserver-maven-plugin/src/test/java/org/mockserver/maven/MockServerRunAndWaitMojoTest.java
@@ -60,7 +60,7 @@ public class MockServerRunAndWaitMojoTest {
         mockServerRunAndWaitMojo.logLevel = "LEVEL";
         mockServerRunAndWaitMojo.timeout = 0;
         when(mockEmbeddedJettyHolder.start(1, 2, 3, 4, "LEVEL")).thenReturn(mockServerRunner);
-        doThrow(new InterruptedException()).when(mockServerRunner).join();
+        doThrow(new InterruptedException("TEST EXCEPTION")).when(mockServerRunner).join();
 
         // when
         mockServerRunAndWaitMojo.execute();
@@ -80,7 +80,7 @@ public class MockServerRunAndWaitMojoTest {
         mockServerRunAndWaitMojo.logLevel = "LEVEL";
         mockServerRunAndWaitMojo.timeout = 2;
         when(mockEmbeddedJettyHolder.start(1, 2, 3, 4, "LEVEL")).thenReturn(mockServerRunner);
-        doThrow(new InterruptedException()).when(mockServerRunner).join(TimeUnit.SECONDS.toMillis(2));
+        doThrow(new InterruptedException("TEST EXCEPTION")).when(mockServerRunner).join(TimeUnit.SECONDS.toMillis(2));
 
         // when
         mockServerRunAndWaitMojo.execute();
