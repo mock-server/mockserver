0a512a597f7570c316ab26a39665e299e079e23b
James Bloom <jamesdbloom@gmail.com>
2014-02-04 02:51:39 +0000

    fixing release script

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # java 1.6 build
-# export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-# mvn release:clean && mvn release:prepare && mvn release:perform
+export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+mvn release:clean && mvn release:prepare && mvn release:perform
 
 if [ $? -eq 0 ]; then
     # java 1.7 build
@@ -10,18 +10,18 @@ if [ $? -eq 0 ]; then
 
     cd mockserver-jetty
     mvn release:clean && mvn release:prepare && mvn release:perform
-    mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-vertx-2.5.pom -Dfile=target/mockserver-vertx-2.5.jar -Dfiles=target/mockserver-vertx-2.5-javadoc.jar,target/mockserver-vertx-2.5-sources.jar,target/org.mock-server~mockserver-vertx~2.5-mod.zip -Dclassifiers=javadoc,sources,mod -Dtypes=jar,jar,zip -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+    mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-vertx-2.6.pom -Dfile=target/mockserver-vertx-2.6.jar -Dfiles=target/mockserver-vertx-2.6-javadoc.jar,target/mockserver-vertx-2.6-sources.jar,target/org.mock-server~mockserver-vertx~2.6-mod.zip -Dclassifiers=javadoc,sources,mod -Dtypes=jar,jar,zip -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
 
     cd ../mockserver-vertx
     mvn release:clean && mvn release:prepare && mvn release:perform
-    mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-vertx-2.5.pom -Dfile=target/mockserver-vertx-2.5.jar -Dfiles=target/mockserver-vertx-2.5-javadoc.jar,target/mockserver-vertx-2.5-sources.jar,target/org.mock-server~mockserver-vertx~2.5-mod.zip -Dclassifiers=javadoc,sources,mod -Dtypes=jar,jar,zip -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+    mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-vertx-2.6.pom -Dfile=target/mockserver-vertx-2.6.jar -Dfiles=target/mockserver-vertx-2.6-javadoc.jar,target/mockserver-vertx-2.6-sources.jar,target/org.mock-server~mockserver-vertx~2.6-mod.zip -Dclassifiers=javadoc,sources,mod -Dtypes=jar,jar,zip -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
 
     cd ../mockserver-examples
     mvn release:clean && mvn release:prepare && mvn release:perform
-    mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-examples-2.5.pom -Dfile=target/mockserver-examples-2.5.war -Dfiles=target/mockserver-examples-2.5-sources.jar,target/mockserver-examples-2.5-javadoc.jar -Dclassifiers=javadoc,sources -Dtypes=jar,jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+    mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-examples-2.6.pom -Dfile=target/mockserver-examples-2.6.war -Dfiles=target/mockserver-examples-2.6-sources.jar,target/mockserver-examples-2.6-javadoc.jar -Dclassifiers=javadoc,sources -Dtypes=jar,jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
 fi
 
-#mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-vertx-2.5.pom -Dfile=target/mockserver-vertx-2.5.jar -Dfiles=target/mockserver-vertx-2.5-javadoc.jar,target/mockserver-vertx-2.5-sources.jar,target/org.mock-server~mockserver-vertx~2.5-mod.zip -Dclassifiers=javadoc,sources,mod -Dtypes=jar,jar,zip -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
-#mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-jetty-2.5.pom -Dfile=target/mockserver-jetty-2.5.jar -Dfiles=target/mockserver-jetty-2.5-sources.jar,target/mockserver-jetty-2.5-javadoc.jar,target/mockserver-jetty-2.5-jar-with-dependencies.jar -Dclassifiers=javadoc,sources,jar-with-dependencies -Dtypes=jar,jar,jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
-#mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-examples-2.5.pom -Dfile=target/mockserver-examples-2.5.jar -Dfiles=target/mockserver-examples-2.5-sources.jar,target/mockserver-examples-2.5-javadoc.jar -Dclassifiers=javadoc,sources -Dtypes=jar,jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+#mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-vertx-2.6.pom -Dfile=target/mockserver-vertx-2.6.jar -Dfiles=target/mockserver-vertx-2.6-javadoc.jar,target/mockserver-vertx-2.6-sources.jar,target/org.mock-server~mockserver-vertx~2.6-mod.zip -Dclassifiers=javadoc,sources,mod -Dtypes=jar,jar,zip -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+#mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-jetty-2.6.pom -Dfile=target/mockserver-jetty-2.6.jar -Dfiles=target/mockserver-jetty-2.6-sources.jar,target/mockserver-jetty-2.6-javadoc.jar,target/mockserver-jetty-2.6-jar-with-dependencies.jar -Dclassifiers=javadoc,sources,jar-with-dependencies -Dtypes=jar,jar,jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
+#mvn gpg:sign-and-deploy-file -DpomFile=target/mockserver-examples-2.6.pom -Dfile=target/mockserver-examples-2.6.jar -Dfiles=target/mockserver-examples-2.6-sources.jar,target/mockserver-examples-2.6-javadoc.jar -Dclassifiers=javadoc,sources -Dtypes=jar,jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging
 
