ac5226ca4f75bcbe8ab82594f0f24a12c896fda5
James Bloom <jamesdbloom@gmail.com>
2014-02-13 23:18:37 +0000

    multiple fixes and improvements

--- mockserver-maven-plugin/src/main/java/org/mockserver/maven/SettableFuture.java
+++ mockserver-maven-plugin/src/main/java/org/mockserver/maven/SettableFuture.java
@@ -4,19 +4,20 @@ import com.google.common.util.concurrent.AbstractFuture;
 
 public class SettableFuture<V> extends AbstractFuture<V> {
 
-  public static <V> SettableFuture<V> create() {
-    return new SettableFuture<V>();
-  }
+    private SettableFuture() {
+    }
 
-  private SettableFuture() {}
+    public static <V> SettableFuture<V> create() {
+        return new SettableFuture<V>();
+    }
 
-  @Override
-  public boolean set(V value) {
-    return super.set(value);
-  }
+    @Override
+    public boolean set(V value) {
+        return super.set(value);
+    }
 
-  @Override
-  public boolean setException(Throwable throwable) {
-    return super.setException(throwable);
-  }
+    @Override
+    public boolean setException(Throwable throwable) {
+        return super.setException(throwable);
+    }
 }
