aca74d3e3b7235fafc886f75794067669b9f77cf
James Bloom <jamesdbloom@gmail.com>
2014-05-18 00:58:08 +0100

    #32 - increased coverage for both unit and integration testing of the request forward logic

--- mockserver-maven-plugin-integration-tests/src/integration-tests/mockserver-starts-stops-server-from-pom-explicitly/src/test/java/org/mockserver/ClientServerMavenPluginIntegrationTest.java
+++ mockserver-maven-plugin-integration-tests/src/integration-tests/mockserver-starts-stops-server-from-pom-explicitly/src/test/java/org/mockserver/ClientServerMavenPluginIntegrationTest.java
@@ -5,21 +5,29 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.mockserver.client.server.MockServerClient;
 import org.mockserver.integration.server.AbstractClientServerIntegrationTest;
+import org.mockserver.integration.testserver.TestServer;
+import org.mockserver.socket.PortFactory;
 
 /**
  * @author jamesdbloom
  */
 public class ClientServerMavenPluginIntegrationTest extends AbstractClientServerIntegrationTest {
 
-    private final static int serverPort = 8080;
-    private final static int serverSecurePort = 8082;
+    private final static int TEST_SERVER_HTTP_PORT = PortFactory.findFreePort();
+    private final static int TEST_SERVER_HTTPS_PORT = PortFactory.findFreePort();
+    private final static int SERVER_HTTP_PORT = 8080;
+    private final static int SERVER_HTTPS_PORT = 8082;
+    private static TestServer testServer = new TestServer();
 
     @BeforeClass
-    public static void startServer() throws Exception {
+    public static void createClient() throws Exception {
         // do nothing maven build should have started server
 
+        // start test server
+        testServer.startServer(TEST_SERVER_HTTP_PORT, TEST_SERVER_HTTPS_PORT);
+
         // start client
-        mockServerClient = new MockServerClient("localhost", serverPort, servletContext);
+        mockServerClient = new MockServerClient("localhost", SERVER_HTTP_PORT, servletContext);
     }
 
     @Before
@@ -27,19 +35,34 @@ public class ClientServerMavenPluginIntegrationTest extends AbstractClientServer
         mockServerClient.reset();
     }
 
-    @Override
-    public int getPort() {
-        return serverPort;
-    }
-
-    @Override
-    public int getSecurePort() {
-        return serverSecurePort;
-    }
-
     @AfterClass
     public static void stopServer() throws Exception {
         // do nothing maven build should stop server
+
+        // stop test server
+        if (testServer != null) {
+            testServer.stop();
+        }
+    }
+
+    @Override
+    public int getMockServerPort() {
+        return SERVER_HTTP_PORT;
+    }
+
+    @Override
+    public int getMockServerSecurePort() {
+        return SERVER_HTTPS_PORT;
+    }
+
+    @Override
+    public int getTestServerPort() {
+        return TEST_SERVER_HTTP_PORT;
+    }
+
+    @Override
+    public int getTestServerSecurePort() {
+        return TEST_SERVER_HTTPS_PORT;
     }
 
 }
