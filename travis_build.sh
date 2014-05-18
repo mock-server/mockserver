f80ffbc7d075bc097487664fee440a756c4aad94
James Bloom <jamesdbloom@gmail.com>
2014-05-18 09:43:38 +0100

    trying to resolve "java.lang.OutOfMemoryError: unable to create new native thread" in Travis

--- travis_build.sh
+++ travis_build.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
-JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
 export MAVEN_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
 export JAVA_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
+JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+current_directory=${PWD}
 
 if [ $JAVA_VER -eq 16 ]; then
     echo
-    echo "-------------------------"
-    echo "------- JAVA 1.6  -------"
-    echo "-------------------------"
+    echo "------------------------"
+    echo "------- JAVA 1.6 -------"
+    echo "------------------------"
     echo
-    git clone -b travis `git config --get remote.origin.url` target/travis
-    mvn deploy --settings target/travis/settings.xml
+    rm -rf $current_directory/target/travis
+    git clone -b travis `git config --get remote.origin.url` $current_directory/target/travis
+    for module in core client integration-testing war proxy-war netty maven-plugin maven-plugin-integration-tests javascript examples; do
+        cd $current_directory/mockserver-$module;
+        echo
+        echo ==== `pwd` ====;
+        echo
+        mvn deploy --settings $current_directory/target/travis/settings.xml
+        echo
+        cd $current_directory;
+    done
 fi
 
 if [ $JAVA_VER -eq 17 ]; then
@@ -20,7 +30,15 @@ if [ $JAVA_VER -eq 17 ]; then
     echo "----- JAVA 1.7 -----"
     echo "--------------------"
     echo
-    mvn test
+    for module in core client integration-testing war proxy-war netty maven-plugin maven-plugin-integration-tests javascript examples; do
+        cd $current_directory/mockserver-$module;
+        echo
+        echo ==== `pwd` ====;
+        echo
+        mvn install
+        echo
+        cd $current_directory;
+    done
 fi
 
 if [ $JAVA_VER -eq 18 ]; then
@@ -29,5 +47,13 @@ if [ $JAVA_VER -eq 18 ]; then
     echo "----- JAVA 1.8 -----"
     echo "--------------------"
     echo
-    mvn test
+    for module in core client integration-testing war proxy-war netty maven-plugin maven-plugin-integration-tests javascript examples; do
+        cd $current_directory/mockserver-$module;
+        echo
+        echo ==== `pwd` ====;
+        echo
+        mvn install
+        echo
+        cd $current_directory;
+    done
 fi
