3f88dce6613f480c0b466c4837b0605762b02c34
James Bloom <jamesdbloom@gmail.com>
2014-06-23 22:16:09 +0100

    adding release script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# java 1.6 build
+if [ $? -eq 0 ]; then
+    export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+    echo
+    echo "-------------------------"
+    echo "------- JAVA 1.6  -------"
+    echo "-------------------------"
+    echo
+    mvn release:clean && mvn release:prepare && mvn release:perform
+fi
