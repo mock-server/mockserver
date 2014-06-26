389e934c4a89a4826733219cea6574e43eb4d152
James Bloom <jamesdbloom@gmail.com>
2014-06-27 00:15:17 +0100

    #51 #50 #49 - add support for json matcher and improving documentation

--- mockserver-maven-plugin/src/main/java/org/mockserver/maven/MockServerRunForkedMojo.java
+++ mockserver-maven-plugin/src/main/java/org/mockserver/maven/MockServerRunForkedMojo.java
@@ -136,13 +136,13 @@ public class MockServerRunForkedMojo extends MockServerAbstractMojo {
 
     @VisibleForTesting
     String getVersion() {
-        String version = "3.0";
+        String version = "3.1";
         try {
             Properties p = new Properties();
             InputStream is = getClass().getResourceAsStream("/META-INF/maven/org.mock-server/mockserver-maven-plugin/pom.properties");
             if (is != null) {
                 p.load(is);
-                version = p.getProperty("version", "3.0");
+                version = p.getProperty("version", "3.1");
             }
         } catch (Exception e) {
             // ignore
