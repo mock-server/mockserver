42c22163e507e95ba0071e0ce9ca4e7b776d426b
James Bloom <jamesdbloom@gmail.com>
2014-07-04 22:07:13 +0100

    added extra test for missing test scenario in json matcher

--- mockserver-core/src/test/java/org/mockserver/matchers/JsonStringMatcherTest.java
+++ mockserver-core/src/test/java/org/mockserver/matchers/JsonStringMatcherTest.java
@@ -118,6 +118,56 @@ public class JsonStringMatcherTest {
     }
 
     @Test
+    public void shouldMatchMatchingSubJsonWithSomeSubJsonFields() {
+        // given
+        String matched = "" +
+                "{\n" +
+                "    \"glossary\": {\n" +
+                "        \"title\": \"example glossary\", \n" +
+                "        \"GlossDiv\": {\n" +
+                "            \"title\": \"S\", \n" +
+                "            \"GlossList\": {\n" +
+                "                \"GlossEntry\": {\n" +
+                "                    \"ID\": \"SGML\", \n" +
+                "                    \"SortAs\": \"SGML\", \n" +
+                "                    \"GlossTerm\": \"Standard Generalized Markup Language\", \n" +
+                "                    \"Acronym\": \"SGML\", \n" +
+                "                    \"Abbrev\": \"ISO 8879:1986\", \n" +
+                "                    \"GlossDef\": {\n" +
+                "                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\", \n" +
+                "                        \"GlossSeeAlso\": [\n" +
+                "                            \"GML\", \n" +
+                "                            \"XML\"\n" +
+                "                        ]\n" +
+                "                    }, \n" +
+                "                    \"GlossSee\": \"markup\"\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+
+        // then
+        assertTrue(new JsonStringMatcher("{\n" +
+                "    \"glossary\": {\n" +
+                "        \"GlossDiv\": {\n" +
+                "            \"title\": \"S\", \n" +
+                "            \"GlossList\": {\n" +
+                "                \"GlossEntry\": {\n" +
+                "                    \"ID\": \"SGML\", \n" +
+                "                    \"Abbrev\": \"ISO 8879:1986\", \n" +
+                "                    \"GlossDef\": {\n" +
+                "                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\"\n" +
+                "                    }, \n" +
+                "                    \"GlossSee\": \"markup\"\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "}").matches(matched));
+    }
+
+    @Test
     public void shouldMatchMatchingSubJsonWithDifferentArrayOrder() {
         // given
         String matched = "" +
