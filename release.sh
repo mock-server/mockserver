edb9285dd6a07511ed0f96d3c531ab8d64c9c101
James Bloom <jamesdbloom@gmail.com>
2014-09-01 02:38:03 +0100

    adding extra memory for release script

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # java 1.6 build
+export MAVEN_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
+export JAVA_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
 if [ $? -eq 0 ]; then
     export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
     echo
