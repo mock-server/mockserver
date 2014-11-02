d26c84d913980c48d3b7cc59c141ce2ea55a6dce
James Bloom <jamesdbloom@gmail.com>
2014-11-02 20:18:41 +0000

    improving the reliability of the stop script

--- scripts/stop_MockServer.sh
+++ scripts/stop_MockServer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ps -ef | grep mockserver | grep -v grep | grep -v $0 | awk '{print $2}' | xargs kill
+`ps -ef | grep mockserver | grep -v grep | grep -v $0 | awk '{print $2}' | xargs -t -I '{}' kill '{}'`; echo done
 
