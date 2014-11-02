5d5d09c56063e4c6c6da5cf37a2095c893dfbf5e
James Bloom <jamesdbloom@gmail.com>
2014-11-02 20:26:40 +0000

    trying to fix release process

--- scripts/stop_MockServer.sh
+++ scripts/stop_MockServer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-`ps -ef | grep mockserver | grep -v grep | grep -v mvn | grep -v $0 | awk '{print $2}' | xargs -t -I '{}' kill '{}'`; echo done
+`ps -ef | grep mockserver | grep -v grep | grep -v mvn | grep -v sonatype-oss-release | grep -v $0 | awk '{print $2}' | xargs -t -I '{}' kill '{}'`; echo done
 
