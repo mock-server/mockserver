68ea9cd2814ac7d25c3198a83fcdd8d0eb5c45e2
James Bloom <jamesdbloom@gmail.com>
2014-12-06 21:08:35 +0000

    added other version of java to script but commented out

--- scripts/quick_build.sh
+++ scripts/quick_build.sh
@@ -5,6 +5,8 @@ export MAVEN_OPTS='-XX:MaxPermSize=1024m -Xmx2048m'
 export JAVA_OPTS='-XX:MaxPermSize=1024m -Xmx2048m'
 # -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=25000
 export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_55.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
 echo
 echo "-------------------------"
 echo "------- JAVA 1.6  -------"
