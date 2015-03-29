6ee7fb5f784a2317920b1690de142853cdd03184
James Bloom <jamesdbloom@gmail.com>
2015-03-29 22:08:36 +0100

    fixing kill scripts to allow for latest version of maven with a different binary name

--- scripts/stop_MockServer.sh
+++ scripts/stop_MockServer.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-`ps -ef | grep mockserver | grep -v grep | grep -v mvn | grep -v sonatype-oss-release | grep -v $0 | awk '{print $2}' | xargs -t -I '{}' kill '{}'`; echo done
-
+`ps -ef | grep mockserver | grep -v grep | grep -v mvn | grep -v maven | grep -v sonatype-oss-release | grep -v $0 | awk '{print $2}' | xargs -t -I '{}' kill '{}'`; echo done
