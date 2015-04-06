02b5ca8f9328bfc555afa0efc804e770db970ae8
James Bloom <jamesdbloom@gmail.com>
2015-04-06 09:08:32 +0100

    fixing release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,5 +10,7 @@ if [ $? -eq 0 ]; then
     echo "------- JAVA 1.6  -------"
     echo "-------------------------"
     echo
-    mvn release:clean -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true" && mvn release:prepare -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true" && mvn release:perform -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true"
+    /usr/local/Cellar/maven/3.2.3/bin/mvn release:clean -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true" && \
+    /usr/local/Cellar/maven/3.2.3/bin/mvn release:prepare -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true" && \
+    /usr/local/Cellar/maven/3.2.3/bin/mvn release:perform -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true"
 fi
