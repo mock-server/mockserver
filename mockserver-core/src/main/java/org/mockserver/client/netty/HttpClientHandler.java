fcec0b01f1b2e88a38ee27baf029eb1bda1f0afd
James Bloom <james.bloom@nektan.com>
2015-04-14 18:30:31 +0100

    #13 fixed issue where NotSslRecord exception was incorrectly logged with SOCKS proxy

--- mockserver-core/src/main/java/org/mockserver/client/netty/HttpClientHandler.java
+++ mockserver-core/src/main/java/org/mockserver/client/netty/HttpClientHandler.java
@@ -3,6 +3,7 @@ package org.mockserver.client.netty;
 import com.google.common.util.concurrent.SettableFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.ssl.NotSslRecordException;
 import org.mockserver.model.HttpResponse;
 
 public class HttpClientHandler extends SimpleChannelInboundHandler<HttpResponse> {
@@ -25,7 +26,9 @@ public class HttpClientHandler extends SimpleChannelInboundHandler<HttpResponse>
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
+        if (!(cause instanceof NotSslRecordException)) {
+            cause.printStackTrace();
+        }
         responseFuture.setException(cause);
         ctx.close();
     }
