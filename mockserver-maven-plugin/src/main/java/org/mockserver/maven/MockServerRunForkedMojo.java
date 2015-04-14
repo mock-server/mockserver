ffbc73398965655b06090e1969e16ea94a0bcbec
James Bloom <jamesdbloom@gmail.com>
2015-04-14 07:50:43 +0100

    upgrading version after release

--- mockserver-maven-plugin/src/main/java/org/mockserver/maven/MockServerRunForkedMojo.java
+++ mockserver-maven-plugin/src/main/java/org/mockserver/maven/MockServerRunForkedMojo.java
@@ -148,13 +148,13 @@ public class MockServerRunForkedMojo extends MockServerAbstractMojo {
 
     @VisibleForTesting
     String getVersion() {
-        String version = "3.9.7";
+        String version = "3.9.8";
         try {
             java.util.Properties p = new java.util.Properties();
             InputStream is = getClass().getResourceAsStream("/META-INF/maven/org.mock-server/mockserver-maven-plugin/pom.properties");
             if (is != null) {
                 p.load(is);
-                version = p.getProperty("version", "3.9.7");
+                version = p.getProperty("version", "3.9.8");
             }
         } catch (Exception e) {
             // ignore
