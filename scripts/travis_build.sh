c3e261a1c2159d4cce91df214995b3e317bcda48
James Bloom <jamesdbloom@gmail.com>
2015-04-27 23:43:01 +0100

    upgrading travis build to java 7 as now has maven version 3.3.1 which requires java 7

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -22,7 +22,9 @@ if [ $JAVA_VER -eq 17 ]; then
     echo "----- JAVA 1.7 -----"
     echo "--------------------"
     echo
-    mvn -q install
+    rm -rf $current_directory/target/travis
+    git clone -b travis `git config --get remote.origin.url` $current_directory/target/travis
+    mvn -q deploy --settings $current_directory/target/travis/settings.xml -Djava.security.egd=file:/dev/./urandom
 fi
 
 if [ $JAVA_VER -eq 18 ]; then
