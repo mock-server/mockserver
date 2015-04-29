66636a27fde28eca1b5b974e9bedd4423d5742c1
Richard Fisk <richard.fisk@gmail.com>
2015-04-29 19:01:37 +1000

    Added binary type and Hashie transform for base-64 body values returned by the mock-server (hosted in docker) when calling retrieve.

--- mockserver-client-ruby/lib/mockserver/model/request.rb
+++ mockserver-client-ruby/lib/mockserver/model/request.rb
@@ -3,7 +3,7 @@ require 'hashie'
 require_relative './parameter'
 require_relative './body'
 require_relative './enum'
-
+require "base64"
 #
 # A class to model a request in an expectation.
 # @author:: Nayyara Samuel (mailto: nayyara.samuel@opower.com)
@@ -17,7 +17,7 @@ module MockServer::Model
   end
 
   # Request model
-  class Request < Hashie::Dash
+  class Request < Hashie::Trash
     include Hashie::Extensions::MethodAccess
     include Hashie::Extensions::IgnoreUndeclared
     include Hashie::Extensions::Coercion
@@ -29,7 +29,11 @@ module MockServer::Model
     property :query_parameters, default: Parameters.new([])
     property :cookies, default: Parameters.new([])
     property :headers, default: Parameters.new([])
-    property :body
+    property :body, transform_with: lambda { |body| 
+      is_base_64_body = body != nil && body.type == :BINARY
+      body_value = is_base_64_body ? Base64.decode64(body.value) : body.value
+      Body.new(type: :STRING, value: body_value)
+    }
 
     coerce_key :method, HTTPMethod
     coerce_key :path, String
