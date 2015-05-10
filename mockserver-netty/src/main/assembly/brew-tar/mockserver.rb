ef3ed0c4c26f2a557c61f140963b036c5f5b66e7
James Bloom <jamesdbloom@gmail.com>
2015-05-10 21:40:55 +0100

    updated changes to mockserver brew Formula

--- mockserver-netty/src/main/assembly/brew-tar/mockserver.rb
+++ mockserver-netty/src/main/assembly/brew-tar/mockserver.rb
@@ -2,7 +2,7 @@ class Mockserver < Formula
   homepage "http://www.mock-server.com/"
   url "https://oss.sonatype.org/content/repositories/releases/org/mock-server/mockserver-netty/3.9.15/mockserver-netty-3.9.15-brew-tar.tar"
   version "3.9.15"
-  sha256 "f4626959a6016cbf347e5f54875a94115d775f73664a35d09277f25d5acf7d47"
+  sha256 "0dbe6e78c3753c1da0381322ec0d348c2a3a02783fd65bf280a7bfaff02046d7"
 
   depends_on :java => "1.6+"
 
@@ -18,5 +18,23 @@ class Mockserver < Formula
     mockserver_log.mkpath
 
     libexec.install_symlink mockserver_log => "log"
+  end
+
+  def test
+    require 'socket'
+
+    server = TCPServer.new(0)
+    port = server.addr[1]
+    server.close
+    mockserver = fork do
+      exec "#{bin}/mockserver", "-serverPort", port.to_s
+    end
+    cmd = "curl -s \"http://localhost:" + port.to_s + "/status\" -X PUT"
+    %x[ #{cmd} ]
+    while $?.exitstatus != 0
+      %x[ #{cmd} ]
+    end
+    system "curl", "http://localhost:" + port.to_s + "/stop", "-X", "PUT"
+    Process.wait(mockserver)
   end
 end
