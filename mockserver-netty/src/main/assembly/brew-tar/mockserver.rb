eb67600db142b23bc8bff28f9ba05a43f98db6fd
James Bloom <jamesdbloom@gmail.com>
2015-06-30 23:18:10 +0100

    version bump after release

--- mockserver-netty/src/main/assembly/brew-tar/mockserver.rb
+++ mockserver-netty/src/main/assembly/brew-tar/mockserver.rb
@@ -1,40 +1,49 @@
 class Mockserver < Formula
+  desc "Mock HTTP server and proxy"
   homepage "http://www.mock-server.com/"
-  url "https://oss.sonatype.org/content/repositories/releases/org/mock-server/mockserver-netty/3.9.16/mockserver-netty-3.9.16-brew-tar.tar"
-  version "3.9.16"
-  sha256 "0dbe6e78c3753c1da0381322ec0d348c2a3a02783fd65bf280a7bfaff02046d7"
+  url "https://oss.sonatype.org/content/repositories/releases/org/mock-server/mockserver-netty/3.9.17/mockserver-netty-3.9.17-brew-tar.tar"
+  version "3.9.17"
+  sha256 "7d7166b752aabeb65a13a66e0f70653d8fd880a72f1c8d80192d771dc63a9eb9"
+
+  bottle do
+    cellar :any
+    sha256 "06500732332e945ded83457a81d68545f8434c7b9a3648e86c0f2219f9737f06" => :yosemite
+    sha256 "be8520628c465c397c0f87f9373573a98cd4e176c66e8ef4319106464341230f" => :mavericks
+    sha256 "1575d2991726bb2685b07dc7f1484b6a991644c68117c4ebeea060752e5c62ce" => :mountain_lion
+  end
 
   depends_on :java => "1.6+"
 
   def install
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/run_mockserver.sh" => "mockserver"
 
-    # add lib directory soft link
     lib.install_symlink "#{libexec}/lib" => "mockserver"
 
-    # add log directory soft link
-    mockserver_log = var/"log"/"mockserver"
+    mockserver_log = var/"log/mockserver"
     mockserver_log.mkpath
 
     libexec.install_symlink mockserver_log => "log"
   end
 
-  def test
-    require 'socket'
+  test do
+    require "socket"
 
     server = TCPServer.new(0)
     port = server.addr[1]
     server.close
+
     mockserver = fork do
       exec "#{bin}/mockserver", "-serverPort", port.to_s
     end
-    cmd = "curl -s \"http://localhost:" + port.to_s + "/status\" -X PUT"
-    %x[ #{cmd} ]
-    while $?.exitstatus != 0
-      %x[ #{cmd} ]
+
+    loop do
+      Utils.popen_read("curl", "-s", "http://localhost:" + port.to_s + "/status", "-X", "PUT")
+      break if $?.exitstatus == 0
     end
-    system "curl", "http://localhost:" + port.to_s + "/stop", "-X", "PUT"
+
+    system "curl", "-s", "http://localhost:" + port.to_s + "/stop", "-X", "PUT"
+
     Process.wait(mockserver)
   end
 end
