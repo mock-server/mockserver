957d5a83f2f635eed863f624dc6d30ac6a4e95da
James Bloom <jamesdbloom@gmail.com>
2015-10-04 15:42:32 +0100

    lowering log level of error messages about configuring loggers using reflection

--- mockserver-core/src/main/java/org/mockserver/configuration/ConfigurationProperties.java
+++ mockserver-core/src/main/java/org/mockserver/configuration/ConfigurationProperties.java
@@ -272,7 +272,7 @@ public class ConfigurationProperties {
                 setLevelMethod.invoke(logger, levelInstance);
             }
         } catch (Exception e) {
-            ConfigurationProperties.logger.info("Exception updating logging level using reflection, likely cause is Logback is not on the classpath");
+            ConfigurationProperties.logger.debug("Exception updating logging level using reflection, likely cause is Logback is not on the classpath");
         }
 
 
@@ -291,7 +291,7 @@ public class ConfigurationProperties {
                 currentLogLevelField.set(logger, logLevelInstance);
             }
         } catch (Exception e) {
-            ConfigurationProperties.logger.info("Exception updating logging level using reflection, likely cause is Logback is not on the classpath");
+            ConfigurationProperties.logger.debug("Exception updating logging level using reflection, likely cause is Logback is not on the classpath");
         }
     }
 
