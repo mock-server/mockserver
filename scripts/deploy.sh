97e51946ce189b57461d0fef067be164fa3cc07f
James Bloom <jamesdbloom@gmail.com>
2016-10-15 06:54:27 +0100

    fixing deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,4 +9,11 @@ echo "-------------------------"
 echo "------- JAVA 1.6  -------"
 echo "-------------------------"
 echo
-/usr/local/Cellar/maven/3.2.3/bin/mvn clean deploy $1
+echo MAVEN_OPTS=$MAVEN_OPTS
+echo
+echo JAVA_OPTS=$JAVA_OPTS
+echo
+echo JAVA_HOME=$JAVA_HOME
+echo
+echo
+/usr/local/Cellar/maven32/3.2.5/bin/mvn clean deploy $1 -Djava.security.egd=file:/dev/./urandom
