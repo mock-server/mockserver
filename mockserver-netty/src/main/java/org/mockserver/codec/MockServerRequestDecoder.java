f10ba07e96dc0e0e964f70eca3a9134a9bf35989
Andy McVitty <andy.mcvitty@gmail.com>
2016-10-25 22:23:11 -0400

    Correcting formatting

--- mockserver-netty/src/main/java/org/mockserver/codec/MockServerRequestDecoder.java
+++ mockserver-netty/src/main/java/org/mockserver/codec/MockServerRequestDecoder.java
@@ -101,8 +101,6 @@ public class MockServerRequestDecoder extends MessageToMessageDecoder<FullHttpRe
                         decodedCookie.value()
                 ));
             }
-   
         }
     }
-
 }
