1acec9bfbee73a5520554b11c57f29bf07786bc9
James Bloom <jamesdbloom@gmail.com>
2017-01-22 16:51:19 +0000

    improving build to prevent pull requests or branches from deploying SNAPSHOTs

--- scripts/snap_ci_build.sh
+++ scripts/snap_ci_build.sh
@@ -3,5 +3,16 @@
 export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 
-mvn -q compile -Dmaven.test.skip=true -DskipTests=true -DskipITs=true
-mvn deploy --settings settings.xml -Djava.security.egd=file:/dev/./urandom
+function branchName {
+    local branch_name=$(git symbolic-ref -q HEAD)
+    branch_name=${branch_name##refs/heads/}
+    branch_name=${branch_name:-HEAD}
+    echo "$branch_name"
+}
+
+mvn -q compile --settings settings.xml -Dmaven.test.skip=true -DskipTests=true -DskipITs=true
+if [ "$(branchName)" = "master" ]; then
+    mvn deploy --settings settings.xml -Djava.security.egd=file:/dev/./urandom
+else
+    mvn install --settings settings.xml -Djava.security.egd=file:/dev/./urandom
+fi
