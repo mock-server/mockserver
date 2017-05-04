06c193543cbdb3e721ccdc0ee70b4139c5196ed8
James Bloom <jamesdbloom@gmail.com>
2017-05-04 20:40:24 +0100

    #330 support inheritance when setting client field

---
+++ mockserver-netty/src/test/java/org/mockserver/junit/MockServerRuleSubClassTest.java
@@ -0,0 +1,25 @@
+package org.mockserver.junit;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockserver.client.server.MockServerClient;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author jamesdbloom
+ */
+public class MockServerRuleSubClassTest extends MockServerRuleSuperClass {
+
+    @Rule
+    public MockServerRule mockServerRule = new MockServerRule(this);
+
+    @Test
+    public void shouldSetTestMockServeField() {
+        assertThat(mockServerClient, is(not(nullValue())));
+    }
+
+}
---
+++ mockserver-netty/src/test/java/org/mockserver/junit/ProxyRuleSubClassTest.java
@@ -0,0 +1,24 @@
+package org.mockserver.junit;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockserver.client.proxy.ProxyClient;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author jamesdbloom
+ */
+public class ProxyRuleSubClassTest extends ProxyRuleSuperClass {
+    @Rule
+    public ProxyRule proxyRule = new ProxyRule(this);
+
+    @Test
+    public void shouldSetTestMockServerField() {
+        assertThat(proxyClient, is(not(nullValue())));
+    }
+
+}
