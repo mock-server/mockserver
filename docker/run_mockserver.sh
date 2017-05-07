95b4cbf8b420e967107e96afb3161eb69c3b4dfe
James Bloom <jamesdbloom@gmail.com>
2017-05-07 19:56:23 +0100

    #273 added support for generic JVM options or system properties being passed to MockServer running in a docker container

--- docker/run_mockserver.sh
+++ docker/run_mockserver.sh
@@ -6,43 +6,43 @@ function showUsage {
     echo >&2 "   run_mockserver.sh [-logLevel <level>] [-serverPort <port>] [-proxyPort <port>] [-proxyRemotePort <port>] [-proxyRemoteHost <hostname>]"
     echo >&2 "                                                                                   "
     echo >&2 "     valid options are:                                                            "
-    echo >&2 "        -logLevel <level>            OFF, ERROR, WARN, INFO, DEBUG, TRACE or ALL, as follows: "
-    echo >&2 "                                     WARN - exceptions and errors                  "
-    echo >&2 "                                     INFO - all interactions                       "
+    echo >&2 "        -logLevel <level>                       OFF, ERROR, WARN, INFO, DEBUG, TRACE or ALL, as follows:"
+    echo >&2 "                                                WARN - exceptions and errors"
+    echo >&2 "                                                INFO - all interactions"
+    echo >&2 "                                                "
+    echo >&2 "        -serverPort <port>                      Specifies the HTTP and HTTPS port for the"
+    echo >&2 "                                                MockServer. Port unification is used to"
+    echo >&2 "                                                support HTTP and HTTPS on the same port"
+    echo >&2 "                                                "
+    echo >&2 "        -proxyPort <port>                       Specifies the HTTP, HTTPS, SOCKS and HTTP"
+    echo >&2 "                                                CONNECT port for proxy. Port unification"
+    echo >&2 "                                                supports for all protocols on the same port"
+    echo >&2 "                                                "
+    echo >&2 "        -proxyRemotePort <port>                 Specifies the port to forward all proxy"
+    echo >&2 "                                                requests to (i.e. all requests received on"
+    echo >&2 "                                                portPort). This setting is used to enable"
+    echo >&2 "                                                the port forwarding mode therefore this"
+    echo >&2 "                                                option disables the HTTP, HTTPS, SOCKS and"
+    echo >&2 "                                                HTTP CONNECT support"
+    echo >&2 "                                                "
+    echo >&2 "        -proxyRemoteHost <hostname>             Specified the host to forward all proxy"
+    echo >&2 "                                                requests to (i.e. all requests received on"
+    echo >&2 "                                                portPort). This setting is ignored unless"
+    echo >&2 "                                                proxyRemotePort has been specified. If no"
+    echo >&2 "                                                value is provided for proxyRemoteHost when"
+    echo >&2 "                                                proxyRemotePort has been specified,"
+    echo >&2 "                                                proxyRemoteHost will default to \"localhost\"."
+    echo >&2 "                                                "
+    echo >&2 "        -genericJVMOptions <system parameters>  Specified generic JVM options or system properties."
     echo >&2 "                                                                                   "
-    echo >&2 "        -serverPort <port>           Specifies the HTTP and HTTPS port for the     "
-    echo >&2 "                                     MockServer. Port unification is used to       "
-    echo >&2 "                                     support HTTP and HTTPS on the same port       "
-    echo >&2 "                                                                                   "
-    echo >&2 "        -proxyPort <port>            Specifies the HTTP, HTTPS, SOCKS and HTTP     "
-    echo >&2 "                                     CONNECT port for proxy. Port unification      "
-    echo >&2 "                                     supports for all protocols on the same port   "
-    echo >&2 "                                                                                   "
-    echo >&2 "        -proxyRemotePort <port>      Specifies the port to forward all proxy       "
-    echo >&2 "                                     requests to (i.e. all requests received on    "
-    echo >&2 "                                     portPort). This setting is used to enable     "
-    echo >&2 "                                     the port forwarding mode therefore this       "
-    echo >&2 "                                     option disables the HTTP, HTTPS, SOCKS and    "
-    echo >&2 "                                     HTTP CONNECT support                          "
-    echo >&2 "                                                                                   "
-    echo >&2 "        -proxyRemoteHost <hostname>  Specified the host to forward all proxy       "
-    echo >&2 "                                     requests to (i.e. all requests received on    "
-    echo >&2 "                                     portPort). This setting is ignored unless     "
-    echo >&2 "                                     proxyRemotePort has been specified. If no     "
-    echo >&2 "                                     value is provided for proxyRemoteHost when    "
-    echo >&2 "                                     proxyRemotePort has been specified,           "
-    echo >&2 "                                     proxyRemoteHost will default to \"localhost\"."
-    echo >&2 "                                                                                   "
-    echo >&2 "   i.e. /opt/mockserver/run_mockserver.sh -logLevel INFO -serverPort 1080 -proxyPort 1090 -proxyRemotePort 80 -proxyRemoteHost www.mock-server.com"
+    echo >&2 "   i.e. /opt/mockserver/run_mockserver.sh -logLevel INFO -serverPort 1080 -proxyPort 1090 -proxyRemotePort 80 -proxyRemoteHost www.mock-server.com -genericJVMOptions \"-Dmockserver.enableCORSForAllResponses=true -Dmockserver.sslSubjectAlternativeNameDomains='org.mock-server.com,mock-server.com'\""
     echo >&2 "                                                                                   "
     exit 1
 }
 
 function runCommand {
     echo
-    printf -v str "%-$((${#1} + 19))s" ' '; echo "${str// /=}"
-    echo "Executing command: $1"
-    printf -v str "%-$((${#1} + 19))s" ' '; echo "${str// /=}"
+    echo "$1"
     echo
     eval $1
 }
@@ -73,6 +73,7 @@ do
         -proxyPort) proxyPort="$2"; shift;;
         -proxyRemotePort) proxyRemotePort="$2"; shift;;
         -proxyRemoteHost) proxyRemoteHost="$2"; shift;;
+        -genericJVMOptions) genericJVMOptions="$2"; shift;;
         -*) notset="true"; break;;
         *) break;;
     esac
@@ -114,10 +115,14 @@ if [ -n "$proxyRemoteHost" ]
 then
     COMMAND_LINE_OPTS="$COMMAND_LINE_OPTS -proxyRemoteHost $proxyRemoteHost"
 fi
+if [ -n "$genericJVMOptions" ]
+then
+    GENERIC_JVM_OPTIONS="$genericJVMOptions"
+fi
 
 if [ -z "$MOCKSERVER_HOME" ]
 then
     MOCKSERVER_HOME="/opt/mockserver"
 fi
 
-runCommand "java -Dfile.encoding=UTF-8 -Dmockserver.logLevel=$LOG_LEVEL -jar $MOCKSERVER_HOME/mockserver-netty-jar-with-dependencies.jar$COMMAND_LINE_OPTS"
+runCommand "java $GENERIC_JVM_OPTIONS -Dfile.encoding=UTF-8 -Dmockserver.logLevel=$LOG_LEVEL -jar $MOCKSERVER_HOME/mockserver-netty-jar-with-dependencies.jar$COMMAND_LINE_OPTS"
