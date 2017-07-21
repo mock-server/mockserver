e425b17423cecb8b65a3d55977f9e03d394596ba
James Bloom <jamesdbloom@gmail.com>
2017-07-21 20:12:11 +0100

    #306 fixing the docker script to deploy tagged versions

--- scripts/local_docker_build_all_tags.sh
+++ scripts/local_docker_build_all_tags.sh
@@ -7,15 +7,14 @@ function runCommand {
     printf -v str "%-$((${#1} + 19))s" ' '; echo "${str// /=}"
     echo "Executing command: $1"
     printf -v str "%-$((${#1} + 19))s" ' '; echo "${str// /=}"
+    eval $1
     echo
-    if [ "$2" == "PUSH" ]; then
-        eval $1
-    fi
 }
 
 for commit in $COMMIT_LIST; do
 	VERSION=`git show $commit:docker/Dockerfile | awk '/mockserver\-netty\-.*\-jar\-with\-dependencies\.jar/{ print $0 }' | grep -o '/mockserver\-netty\-[0-9][0-9\.]*\-jar\-with\-dependencies\.jar' | egrep -o '[0-9][0-9\.]*'`
     echo $commit, $VERSION;
-    runCommand "docker build docker" $1
+    runCommand "docker build -t jamesdbloom/mockserver:mockserver-$VERSION ./docker"
+#    runCommand "docker push jamesdbloom/mockserver:mockserver-$VERSION"
     echo
 done
