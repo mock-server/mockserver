88e084f4a292464f1b455053787c5ca416a32544
James Bloom <jamesdbloom@gmail.com>
2017-07-25 07:33:03 +0100

    #306 fixing the docker script for tagged version to allow for github TLS changes and wget not being compatible - second attempt

--- scripts/local_docker_build_all_tags.sh
+++ scripts/local_docker_build_all_tags.sh
@@ -14,8 +14,12 @@ function runCommand {
 for commit in $COMMIT_LIST; do
 	VERSION=`git show $commit:docker/Dockerfile | awk '/mockserver\-netty\-.*\-jar\-with\-dependencies\.jar/{ print $0 }' | grep -o '/mockserver\-netty\-[0-9][0-9\.]*\-jar\-with\-dependencies\.jar' | egrep -o '[0-9][0-9\.]*'`
     echo $commit, $VERSION;
+    cp docker/Dockerfile ../Dockerfile_mockserver
     git checkout "$commit"
-    sed -i '' -e 's/FROM java/FROM openjdk\:alpine/g' docker/Dockerfile
+    cp ../Dockerfile_mockserver docker/Dockerfile
+    git reset HEAD --hard
+#    sed -i '' -e 's/FROM java/FROM openjdk\:alpine/g' docker/Dockerfile
+
     runCommand "docker build -t jamesdbloom/mockserver:mockserver-$VERSION ./docker"
 #    runCommand "docker push jamesdbloom/mockserver:mockserver-$VERSION"
     echo
