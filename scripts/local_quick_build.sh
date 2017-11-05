ca7a2ac22c47d668a086c3f1301fad30433cc26a
James Bloom <jamesdbloom@gmail.com>
2017-11-05 10:30:41 +0000

    making it clearer which version of java is being used when running script

--- scripts/local_quick_build.sh
+++ scripts/local_quick_build.sh
@@ -10,7 +10,12 @@ export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 # -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=25000
 export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
-
-echo "requires maven version 3.2.5 so that Java 1.6 can be used, i.e. brew install maven@3.2"
+echo
+echo "======================================================================================"
+echo "Requires maven version 3.2.5 so that Java 1.6 can be used, i.e. brew install maven@3.2"
+echo "======================================================================================"
+echo
+java -version
+echo
 
 mvn clean install $1 -Dmaven-invoker-parallel-threads=4 -Djava.security.egd=file:/dev/./urandom
