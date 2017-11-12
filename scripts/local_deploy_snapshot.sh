38b4e72ffb038d26a8f9e59dd3c1f68a111d474f
James Bloom <jamesdbloom@gmail.com>
2017-11-12 09:34:32 +0000

    harmonised snapshot deploy and quick build scripts

--- scripts/local_deploy_snapshot.sh
+++ scripts/local_deploy_snapshot.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 # java 1.6 build
-export MAVEN_OPTS='-XX:MaxPermSize=1024m -Xmx2048m'
-export JAVA_OPTS='-XX:MaxPermSize=1024m -Xmx2048m'
+export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
+export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+echo
+echo "======================================================================================"
+echo "Requires maven version 3.2.5 so that Java 1.6 can be used, i.e. brew install maven@3.2"
+echo "======================================================================================"
+echo
+java -version
+echo
+
+export PATH=/usr/local/Cellar/maven@3.2/3.2.5/bin:$PATH
 
 mvn clean deploy $1 -Djava.security.egd=file:/dev/./urandom
