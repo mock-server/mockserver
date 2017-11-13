7d2bb207a220758e902097379b2b09b608fbb707
James Bloom <jamesdbloom@gmail.com>
2017-11-13 06:51:25 +0000

    adding mvn version output

--- scripts/local_quick_build.sh
+++ scripts/local_quick_build.sh
@@ -5,6 +5,7 @@ export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 # -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=25000
 export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+export PATH=/usr/local/Cellar/maven@3.2/3.2.5/bin:$PATH
 echo
 echo "======================================================================================"
 echo "Requires maven version 3.2.5 so that Java 1.6 can be used, i.e. brew install maven@3.2"
@@ -12,7 +13,8 @@ echo "==========================================================================
 echo
 java -version
 echo
+mvn -version
+echo
 
-export PATH=/usr/local/Cellar/maven@3.2/3.2.5/bin:$PATH
 
-mvn clean install $1 -Dmaven-invoker-parallel-threads=4 -Djava.security.egd=file:/dev/./urandom
+mvn clean install $1 -Dmaven-invoker-parallel-threads=2 -Djava.security.egd=file:/dev/./urandom
