173fb79aa46547603c937b5a3d90d3d834a6bed6
James Bloom <jamesdbloom@gmail.com>
2017-11-16 21:30:07 +0000

    fixed release script to ensure it uses the correct version of maven

--- scripts/local_release.sh
+++ scripts/local_release.sh
@@ -3,8 +3,18 @@
 # java 1.6 build
 export MAVEN_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
 export JAVA_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
+export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+export PATH=/usr/local/Cellar/maven@3.2/3.2.5/bin:$PATH
+echo
+echo "======================================================================================"
+echo "Requires maven version 3.2.5 so that Java 1.6 can be used, i.e. brew install maven@3.2"
+echo "======================================================================================"
+echo
+java -version
+echo
+mvn -version
+echo
 if [ $? -eq 0 ]; then
-    export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
     mvn release:clean -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true" && \
     mvn release:prepare -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true" && \
     mvn release:perform -Drelease.arguments="-DnonReleaseBuild=false -Dmaven.test.skip=true -DskipTests=true"
