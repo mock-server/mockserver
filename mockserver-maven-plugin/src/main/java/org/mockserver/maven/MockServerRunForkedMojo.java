3ede518ab0c4a6e3262781c5a78bbf0735ca139b
James D Bloom <jamesdbloom@gmail.com>
2017-12-18 09:40:56 +0000

    updated the versions to reflect newer release

--- mockserver-maven-plugin/src/main/java/org/mockserver/maven/MockServerRunForkedMojo.java
+++ mockserver-maven-plugin/src/main/java/org/mockserver/maven/MockServerRunForkedMojo.java
@@ -30,7 +30,7 @@ import java.util.concurrent.TimeUnit;
  *
  * To run from command line:
  *
- *    mvn -Dmockserver.serverPort="1080" -Dmockserver.proxyPort="1090" -Dmockserver.logLevel="TRACE" org.mock-server:mockserver-maven-plugin:4.0.0:runForked
+ *    mvn -Dmockserver.serverPort="1080" -Dmockserver.proxyPort="1090" -Dmockserver.logLevel="TRACE" org.mock-server:mockserver-maven-plugin:5.2.2:runForked
  *
  * @author jamesdbloom
  */
@@ -174,13 +174,13 @@ public class MockServerRunForkedMojo extends MockServerAbstractMojo {
 
     @VisibleForTesting
     String getVersion() {
-        String version = "4.0.0";
+        String version = "5.2.2";
         try {
             java.util.Properties p = new java.util.Properties();
             InputStream is = getClass().getResourceAsStream("/META-INF/maven/org.mock-server/mockserver-maven-plugin/pom.properties");
             if (is != null) {
                 p.load(is);
-                version = p.getProperty("version", "4.0.0");
+                version = p.getProperty("version", "5.2.2");
             }
         } catch (Exception e) {
             // ignore
