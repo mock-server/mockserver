58aedbdb61c585a0f231eebcec9993b4a4feaac1
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2018-11-15 22:56:58 +0000

    minor changes to improve build performance

--- scripts/local_quick_build.sh
+++ scripts/local_quick_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
+MAVEN_OPTS= -XX:+TieredCompilation -XX:TieredStopAtLevel=1
 export JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=1024m -Xmx2048m"
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 echo
@@ -10,4 +11,4 @@ mvn -version
 echo
 
 # to run from specific module use argument in quotes "-rf mockserver-war"
-mvn clean install $1 -Djava.security.egd=file:/dev/urandom
+mvn -T 1C clean install -offline $1 -Djava.security.egd=file:/dev/urandom
