a63c64aeee702057c0216868925af8712a48bbfc
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2018-12-23 07:54:15 +0000

    added clarification on node logging to website

--- scripts/local_generate_web_site.sh
+++ scripts/local_generate_web_site.sh
@@ -13,7 +13,5 @@ cd _site
 cp mock_server/mockserver_clients.html .
 cp mock_server/running_mock_server.html .
 cp mock_server/debugging_issues.html .
-cp mock_server/creating_expectations_request_matchers.html .
 cp mock_server/creating_expectations.html .
-cp mock_server/creating_expectations_java_example_code.html .
 #zip -r mockserver_site.zip .
