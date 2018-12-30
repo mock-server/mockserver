c492eaa24964a5a5c3b8ffa7a8f2f98787fb00ec
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2018-12-30 07:02:27 +0000

    simplified website generation scripts

--- scripts/local_generate_web_site.sh
+++ scripts/local_generate_web_site.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-export MAVEN_OPTS='-XX:MaxPermSize=1024m -Xmx1024m'
-
-rm -rf jekyll-www.mock-server.com/_site
-rm -rf jekyll-www.mock-server.com/apidocs
-
-mvn javadoc:aggregate -DreportOutputDirectory='${basedir}/jekyll-www.mock-server.com'
+rm -rf jekyll-www.mock-server.com/_site || true
 
 cd jekyll-www.mock-server.com
 bundle exec jekyll build
@@ -14,4 +9,3 @@ cp mock_server/mockserver_clients.html .
 cp mock_server/running_mock_server.html .
 cp mock_server/debugging_issues.html .
 cp mock_server/creating_expectations.html .
-#zip -r mockserver_site.zip .
