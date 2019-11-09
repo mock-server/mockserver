3dde37751ba2d3f69b5ff7d6568c03d71e6b95e2
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-11-09 15:27:41 +0000

    removing unused import

--- mockserver-netty/src/main/java/org/mockserver/dashboard/serializers/LogEntryDTOSerializer.java
+++ mockserver-netty/src/main/java/org/mockserver/dashboard/serializers/LogEntryDTOSerializer.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.mockserver.dashboard.model.LogEntryDTO;
 
 import java.io.IOException;
-import java.util.Base64;
 
 /**
  * @author jamesdbloom
