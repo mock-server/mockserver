6510cf396e69c083c3464fc4ce8b483cfaa55a66
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-11-26 22:15:47 +0000

    improved old docker tag creation script

--- scripts/local_docker_push_tag.sh
+++ scripts/local_docker_push_tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-COMMIT_LIST=`git log --all -G"jar\-with\-dependencies" --grep "version" --grep "update" --grep "upgrading" --grep "updating" --format=format:'%h' -- 'docker/Dockerfile'`
+COMMIT_LIST="mockserver-5.0.0 mockserver-5.0.1 mockserver-5.1.0 mockserver-5.1.1 mockserver-5.2.0 mockserver-5.2.1 mockserver-5.2.2 mockserver-5.2.3 mockserver-5.3.0 mockserver-5.4.1 mockserver-5.5.0 mockserver-5.5.1 mockserver-5.5.2 mockserver-5.5.3 mockserver-5.5.4 mockserver-5.6.0 mockserver-5.6.1 mockserver-5.7.0 mockserver-5.7.1 mockserver-5.7.2"
 
 function runCommand {
     echo
@@ -11,6 +11,20 @@ function runCommand {
     echo
 }
 
-runCommand "docker build -t jamesdbloom/mockserver:mockserver-$1 ./docker"
-echo "proceed?" && read && echo "are you sure?" && read
-runCommand "docker push jamesdbloom/mockserver:mockserver-$1"
+cd mockserver
+for commit in $COMMIT_LIST; do
+	VERSION=`echo $commit | egrep -o '([0-9]+\.){2}[0-9]+'`
+    echo $commit, $VERSION;
+    git checkout "$commit"
+    sed -E "s|([0-9]+\.){2}[0-9]+|${VERSION}|g" docker/Dockerfile > docker/Dockerfile_updated
+    sed s/--max-redirect=5/--max-redirect=15/g docker/Dockerfile_updated > docker/Dockerfile_updated_two    
+    sed s/--max-redirect=1/--max-redirect=15/g docker/Dockerfile_updated > docker/Dockerfile_updated_three    
+    cp docker/Dockerfile_updated_three docker/Dockerfile
+    runCommand "docker build -t mockserver/mockserver:mockserver-$VERSION ./docker"
+    runCommand "docker push mockserver/mockserver:mockserver-$VERSION"
+    git reset HEAD --hard
+    git checkout master
+    git add -A
+    git reset HEAD --hard
+    echo
+done
