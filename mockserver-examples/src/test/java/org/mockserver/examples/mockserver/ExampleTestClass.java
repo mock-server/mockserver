111640ee7b90fcb947613a245a8462b9ddc436a1
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-12-08 16:00:44 +0000

    added extra TLS / HTTPS documentation to website

--- mockserver-examples/src/test/java/org/mockserver/examples/mockserver/ExampleTestClass.java
+++ mockserver-examples/src/test/java/org/mockserver/examples/mockserver/ExampleTestClass.java
@@ -1,36 +1,34 @@
-package org.mockserver.examples.proxy;
+package org.mockserver.examples.mockserver;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.logging.MockServerLogger;
-import org.mockserver.model.Format;
 import org.mockserver.socket.PortFactory;
 import org.mockserver.socket.tls.KeyStoreFactory;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class AnalyseSystemExampleTestClass {
+public class ExampleTestClass {
 
-    private static ClientAndServer proxy;
+    private static ClientAndServer mockServer;
 
     @BeforeClass
-    public static void startProxyAndServer() {
+    public static void startMockServer() {
         // ensure all connection using HTTPS will use the SSL context defined by
         // MockServer to allow dynamically generated certificates to be accepted
         HttpsURLConnection.setDefaultSSLSocketFactory(new KeyStoreFactory(new MockServerLogger()).sslContext().getSocketFactory());
-        proxy = ClientAndServer.startClientAndServer(PortFactory.findFreePort());
+        mockServer = ClientAndServer.startClientAndServer(PortFactory.findFreePort());
     }
 
     @AfterClass
-    public static void stopServerAndProxy() {
-        proxy.retrieveRecordedExpectations(null, Format.JAVA);
-        proxy.stop();
+    public static void stopMockServer() {
+        mockServer.stop();
     }
 
     @Test
     public void shouldDoSomething() {
-        // send requests to system being analysed
+        // test system
     }
 }
