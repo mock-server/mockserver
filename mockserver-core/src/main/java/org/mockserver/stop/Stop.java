b918c843bf6c81943ef11f0eb875f440fbb298d5
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2019-12-31 15:50:21 +0000

    renamed variable

--- mockserver-core/src/main/java/org/mockserver/stop/Stop.java
+++ mockserver-core/src/main/java/org/mockserver/stop/Stop.java
@@ -10,12 +10,12 @@ public class Stop {
 
     private static final Logger logger = LoggerFactory.getLogger(Stop.class);
 
-    public static void stopQuietly(Stoppable lifeCycle) {
-        if (lifeCycle != null) {
+    public static void stopQuietly(Stoppable stoppable) {
+        if (stoppable != null) {
             try {
-                lifeCycle.stop();
+                stoppable.stop();
             } catch (Throwable throwable) {
-                logger.debug("Exception stopping " + lifeCycle, throwable);
+                logger.debug("Exception stopping " + stoppable, throwable);
             }
         }
     }
