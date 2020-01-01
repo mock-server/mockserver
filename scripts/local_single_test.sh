5b7cfc9185312d045a0ae1b237096f50f23bde6e
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2020-01-01 07:26:10 +0000

    added script to easily run single test from maven

---
+++ scripts/local_single_test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+export MAVEN_OPTS="$MAVEN_OPTS -Xms2048m -Xmx8192m"
+export JAVA_OPTS="$JAVA_OPTS -Xms2048m -Xmx8192m"
+export JAVA_HOME=`/usr/libexec/java_home -v 12`
+echo
+java -version
+echo
+./mvnw -version
+echo
+
+# to run from specific test use argument in quotes "ExpectationFileWatcherIntegrationTest" or "ExpectationFileWatcherIntegrationTest#shouldDetectModifiedInitialiserJsonOnAdd"
+./mvnw -T 1C -Dtest="none" -Dit.test="$1" install -DskipAssembly=true -DfailIfNoTests=false
