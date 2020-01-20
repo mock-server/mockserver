1297f2199e98367a09964fd1f1b289ec7b655957
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2020-01-20 11:12:30 +0000

    example test showing blocking error for forward callbacks

--- mockserver-netty/src/test/java/org/mockserver/netty/integration/mock/WebsocketCallbackRegistryIntegrationTest.java
+++ mockserver-netty/src/test/java/org/mockserver/netty/integration/mock/WebsocketCallbackRegistryIntegrationTest.java
@@ -4,12 +4,27 @@ import org.hamcrest.CoreMatchers;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 import org.mockserver.client.MockServerClient;
+import org.mockserver.configuration.ConfigurationProperties;
 import org.mockserver.integration.ClientAndServer;
-import org.mockserver.testing.integration.mock.AbstractMockingIntegrationTestBase;
 import org.mockserver.metrics.Metrics;
+import org.mockserver.model.Header;
+import org.mockserver.testing.integration.mock.AbstractMockingIntegrationTestBase;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import static org.mockserver.matchers.Times.once;
+import static org.mockserver.model.ConnectionOptions.connectionOptions;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 import static org.mockserver.model.HttpStatusCode.OK_200;
@@ -329,6 +344,80 @@ public class WebsocketCallbackRegistryIntegrationTest extends AbstractMockingInt
                     headersToIgnore)
             );
             objectCallbackCounter++;
+        }
+    }
+
+    @Test
+    @Ignore
+    public void shouldForwardModifiedRequestAndReturnModifiedResponse() throws Exception {
+        ConfigurationProperties.logLevel("WARN");
+        ClientAndServer proxy = null;
+        try {
+            mockServerClient
+                .when(
+                    request()
+                        .withMethod("GET")
+                        .withPath("/api/v1/employees")
+                )
+                .respond(
+                    response()
+                        .withBody("original body response")
+                        .withConnectionOptions(
+                            connectionOptions()
+                                .withSuppressContentLengthHeader(true)
+                                .withCloseSocket(true)
+                        )
+                );
+            String addedHeader = UUID.randomUUID().toString();
+            proxy = startClientAndServer();
+            proxy
+                .when(
+                    request()
+                        .withPath("/api/v1/employees")
+                )
+                .forward(
+                    httpRequest -> httpRequest
+                        .clone()
+                        .replaceHeader(new Header("host", "localhost:" + (Integer) getServerPort())),
+                    (httpRequest, httpResponse) ->
+                        httpResponse
+                            .withBody("modified body response " + httpRequest.getFirstHeader("Counter"))
+                            .withHeader("AddedHeader", addedHeader)
+                            .removeHeader("Content-Length")
+                );
+
+            for (int counter = 0; counter < 250; ++counter) {
+                System.out.println("Running test :" + counter);
+                try {
+                    URL url = new URL("http://localhost:" + proxy.getLocalPort() + "/api/v1/employees");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setRequestProperty("Counter", "" + counter);
+                    int responseCode = con.getResponseCode();
+                    StringBuilder textBuilder = new StringBuilder();
+                    try (Reader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), Charset.forName(StandardCharsets.UTF_8.name())))) {
+                        int c;
+                        while ((c = reader.read()) != -1) {
+                            textBuilder.append((char) c);
+                        }
+                    }
+                    String body = textBuilder.toString();
+                    con.disconnect();
+
+                    // then
+                    assertThat(responseCode, is(200));
+                    assertThat(con.getHeaderField("AddedHeader"), is(addedHeader));
+                    assertThat(body, is("modified body response " + counter));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw e;
+                }
+                System.out.println("Finishing test :" + counter);
+            }
+        } finally {
+            if (proxy != null) {
+                proxy.close();
+            }
         }
     }
 }
