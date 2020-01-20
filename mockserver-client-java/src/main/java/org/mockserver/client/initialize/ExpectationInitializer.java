9f6cbfe45e96e9ddc506a68168c3b0570b5c8c30
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2020-01-20 17:28:48 +0000

    improved clarity for different expectation initializer classes

--- mockserver-client-java/src/main/java/org/mockserver/client/initialize/ExpectationInitializer.java
+++ mockserver-client-java/src/main/java/org/mockserver/client/initialize/ExpectationInitializer.java
@@ -4,7 +4,9 @@ import org.mockserver.client.MockServerClient;
 
 /**
  * @author jamesdbloom
+ * @deprecated use org.mockserver.client.initialize.ClientExpectationInitializer instead
  */
+@Deprecated
 public interface ExpectationInitializer {
 
     void initializeExpectations(MockServerClient mockServerClient);
---
+++ mockserver-client-java/src/main/java/org/mockserver/client/initialize/PluginExpectationInitializer.java
@@ -0,0 +1,20 @@
+package org.mockserver.client.initialize;
+
+import org.mockserver.client.MockServerClient;
+
+/**
+ * If the MockServer is started using the Maven Plugin a initializationClass property can be specified to initialize expectations, when the MockServer starts.
+ *
+ * Note: the plugin must be started during the process-test-classes to ensure that the initialization class has been compiled from either src/main/java or
+ * src/test/java locations. In addition the initializer can only be used with start and run goals, it will not work with the runForked goal as a JVM is forked
+ * with a separate classpath. (required: false, default: false)
+ *
+ * See: http://mock-server.com/mock_server/initializing_expectations.html#maven_plugin_expectation_initializer_class
+ *
+ * @author jamesdbloom
+ */
+public interface PluginExpectationInitializer {
+
+    void initializeExpectations(MockServerClient mockServerClient);
+
+}
