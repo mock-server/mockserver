e8ffa543fa78045511e1abd75fe25aa0ec9a624e
James Bloom <733179+jamesdbloom@users.noreply.github.com>
2020-02-02 07:36:25 +0000

    improve menu scrolling and where section of documentation

--- jekyll-www.mock-server.com/scripts/toggle_menu.js
+++ jekyll-www.mock-server.com/scripts/toggle_menu.js
@@ -34,11 +34,23 @@
 
 }(this, this.document));
 
-function scrollActiveMenuItemIntoView() {
-    var activeMenuItem = document.querySelector("li.active");
-    if (activeMenuItem) {
+var scrollActiveMenuItemIntoView = function() {
+    var activeMenuItem = document.querySelector("#menu li.active");
+    if (activeMenuItem && !isInViewport(activeMenuItem)) {
         activeMenuItem.scrollIntoView({inline: 'nearest', behaviour: 'auto'});
     }
-}
+};
+
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 window.onload = scrollActiveMenuItemIntoView;
+
+document.addEventListener("DOMContentLoaded", scrollActiveMenuItemIntoView);
