eeeb1750aef3be33382e6c9b7c9383713fb86435
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-08-03 14:57:38 +0200

    #767 Add remaining tests and remove Spring Boot Dependency

--- mockserver-spring/src/test/java/org/mockserver/spring/MockServerPropertyCustomizerTest.java
+++ mockserver-spring/src/test/java/org/mockserver/spring/MockServerPropertyCustomizerTest.java
@@ -3,15 +3,13 @@ package org.mockserver.spring;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @MockServerTest("server.url=http://localhost:${mockServerPort}/")
 public class MockServerPropertyCustomizerTest {
@@ -19,7 +17,7 @@ public class MockServerPropertyCustomizerTest {
     @Value("${server.url}")
     String serverUrl;
 
-    @Value("${mockServerPort}")
+    @MockServerPort
     Integer mockServerPort;
 
     @Test
