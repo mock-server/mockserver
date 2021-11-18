c4331b55fa4f2b5ae76ca2ce2c2944c435069cff
Cheizoo, W.F. (Willem) <willem.cheizoo@devolksbank.nl>
2021-11-18 20:50:45 +0100

    #1104 Add support for inherited use of @MockServerTest. Where all the annotations from the inherited classes are processed as properties.

--- mockserver-spring-test-listener/src/test/java/org/mockserver/springtest/MockServerPropertyCustomizerTest.java
+++ mockserver-spring-test-listener/src/test/java/org/mockserver/springtest/MockServerPropertyCustomizerTest.java
@@ -1,28 +1,33 @@
 package org.mockserver.springtest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.junit4.SpringRunner;
+
 @RunWith(SpringRunner.class)
-@MockServerTest("server.url=http://localhost:${mockServerPort}/")
-public class MockServerPropertyCustomizerTest {
+@MockServerTest("server.url.path-b=http://localhost:${mockServerPort}/b")
+public class MockServerPropertyCustomizerTest extends MockServerPropertyCustomizerAbstractTest {
 
-    @Value("${server.url}")
-    private String serverUrl;
+  @Value("${server.url.path-b}")
+  private String serverUrlPathB;
 
-    @MockServerPort
-    private Integer mockServerPort;
+  @MockServerPort private Integer mockServerPort;
 
-    @Test
-    public void mockServerPortIsReplaced() {
-        assertThat(serverUrl, is(not(nullValue())));
-        assertThat(serverUrl, is("http://localhost:" + mockServerPort + "/"));
-    }
+  @Test
+  public void mockServerPortIsReplaced() {
+    assertThat(serverUrlPathB, is(not(nullValue())));
+    assertThat(serverUrlPathB, is("http://localhost:" + mockServerPort + "/b"));
+  }
+
+  @Test
+  public void mockServerPortIsReplacedWithSuperClass() {
+    assertThat(serverUrlPathA, is(not(nullValue())));
+    assertThat(serverUrlPathA, is("http://localhost:" + mockServerPort + "/a"));
+  }
 }
