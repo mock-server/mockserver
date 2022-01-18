2dc6805f7c79435f48a6624d81843d9640ed720a
Slawomir Luty <Slawomir.Luty@infobip.com>
2022-01-18 16:05:54 +0100

    fixed tests to use new method signature

--- mockserver-spring-test-listener/src/test/java/org/mockserver/springtest/MockServerPropertyCustomizerEqualityTest.java
+++ mockserver-spring-test-listener/src/test/java/org/mockserver/springtest/MockServerPropertyCustomizerEqualityTest.java
@@ -2,6 +2,9 @@ package org.mockserver.springtest;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
@@ -10,48 +13,48 @@ public class MockServerPropertyCustomizerEqualityTest {
 
     @Test
     public void hashCodeShouldBeTheSameForNoParamsInConstructor() {
-        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer();
-        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer();
+        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer(Collections.emptyList());
+        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer(Collections.emptyList());
 
         assertThat(actual.hashCode(), is(expected.hashCode()));
     }
 
     @Test
     public void hashCodeShouldBeTheSameForClassWithTheSameParamsInConstructor() {
-        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer("A", "B");
-        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer("A", "B");
+        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer(Arrays.asList("A", "B"));
+        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer(Arrays.asList("A", "B"));
 
         assertThat(actual.hashCode(), is(expected.hashCode()));
     }
 
     @Test
     public void hashCodeShouldBeDifferentForDifferentParamsInConstructor() {
-        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer("A", "B");
-        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer("A", "C");
+        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer(Arrays.asList("A", "B"));
+        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer(Arrays.asList("A", "C"));
 
         assertThat(actual.hashCode(), is(not(expected.hashCode())));
     }
 
     @Test
     public void shouldBeEqualForObjectsWithoutParamsInConstructor() {
-        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer();
-        MockServerPropertyCustomizer p2 = new MockServerPropertyCustomizer();
+        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer(Collections.emptyList());
+        MockServerPropertyCustomizer p2 = new MockServerPropertyCustomizer(Collections.emptyList());
 
         assertThat(actual, is(p2));
     }
 
     @Test
     public void shouldBeEqualForObjectsWithSameParamsInConstructor() {
-        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer("A", "B");
-        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer("A", "B");
+        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer(Arrays.asList("A", "B"));
+        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer(Arrays.asList("A", "B"));
 
         assertThat(actual, is(expected));
     }
 
     @Test
     public void shouldNotBeEqualForObjectsWithDifferentParamsInConstructor() {
-        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer("A", "B");
-        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer("A", "C");
+        MockServerPropertyCustomizer actual = new MockServerPropertyCustomizer(Arrays.asList("A", "B"));
+        MockServerPropertyCustomizer expected = new MockServerPropertyCustomizer(Arrays.asList("A", "C"));
 
         assertThat(actual, is(not(expected)));
     }
