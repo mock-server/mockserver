16e690cfacf3bbe2468ecc8c74baa58cd497f528
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-01-19 12:07:36 +0000

    removed duplicate script

--- scripts/local_buildkite_build.sh
+++ scripts/local_buildkite_build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+docker pull mockserver/mockserver:maven
 docker run -v $(pwd):/build/mockserver -w /build/mockserver -a stdout -a stderr -e BUILDKITE_BRANCH=$BUILDKITE_BRANCH mockserver/mockserver:maven /build/mockserver/scripts/buildkite_quick_build.sh
--- scripts/local_docker_build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull mockserver/mockserver:maven
-docker run --memory=4096m --oom-kill-disable -v `pwd`:/mockserver -w /mockserver -a stdout -a stderr mockserver/mockserver:maven /mockserver/scripts/buildkite_quick_build.sh
