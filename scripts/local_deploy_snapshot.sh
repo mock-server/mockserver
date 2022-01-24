8919555f461250ba582775c71c8ad0f079a337d4
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-01-24 06:46:04 +0000

    Removed incorrect back ticks from JAVA_HOME

--- scripts/local_deploy_snapshot.sh
+++ scripts/local_deploy_snapshot.sh
@@ -5,8 +5,8 @@ set -e
 export MAVEN_OPTS="$MAVEN_OPTS -Xms8192m -Xmx8192m"
 export JAVA_OPTS="$JAVA_OPTS -Xms8192m -Xmx8192m"
 # line below no longer appears to be working so using less resilient hard coded location
-#export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export JAVA_HOME=`/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_201.jdk/Contents/Home
 echo
 java -version
 echo
