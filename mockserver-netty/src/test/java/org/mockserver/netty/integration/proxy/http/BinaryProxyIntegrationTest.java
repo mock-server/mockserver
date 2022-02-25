ff0be1150ac3a27317688c8fb5abfc4432b7509f
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-02-25 12:52:14 +0000

    fixed flakiness of binary proxy test

--- mockserver-netty/src/test/java/org/mockserver/netty/integration/proxy/http/BinaryProxyIntegrationTest.java
+++ mockserver-netty/src/test/java/org/mockserver/netty/integration/proxy/http/BinaryProxyIntegrationTest.java
@@ -3,8 +3,8 @@ package org.mockserver.netty.integration.proxy.http;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockserver.httpclient.NettyHttpClient;
 import org.mockserver.integration.ClientAndServer;
@@ -18,6 +18,7 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import static java.util.concurrent.TimeUnit.*;
@@ -33,7 +34,12 @@ import static org.slf4j.event.Level.ERROR;
  */
 public class BinaryProxyIntegrationTest {
 
-    private static final EventLoopGroup clientEventLoopGroup = new NioEventLoopGroup(3, new Scheduler.SchedulerThreadFactory(BinaryProxyIntegrationTest.class.getSimpleName() + "-eventLoop"));
+    private static EventLoopGroup clientEventLoopGroup;
+
+    @BeforeClass
+    public static void startEventLoopGroup() {
+        clientEventLoopGroup = new NioEventLoopGroup(2, new Scheduler.SchedulerThreadFactory(BinaryProxyIntegrationTest.class.getSimpleName() + "-eventLoop"));
+    }
 
     @AfterClass
     public static void stopEventLoopGroup() {
@@ -43,13 +49,14 @@ public class BinaryProxyIntegrationTest {
     @Test
     public void shouldForwardBinaryMessages() throws Exception {
         // given
-        byte[] randomRequestBytes = RandomUtils.nextBytes(100);
-        byte[] randomResponseBytes = RandomUtils.nextBytes(100);
+        byte[] randomRequestBytes = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
+        byte[] randomResponseBytes = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
+        ClientAndServer proxyClientAndServer = null;
         try {
             try (ServerSocket serverSocket = new ServerSocket(0)) {
                 // and
                 int serverSocketPort = serverSocket.getLocalPort();
-                ClientAndServer proxyClientAndServer = startClientAndServer("127.0.0.1", serverSocketPort);
+                proxyClientAndServer = startClientAndServer("127.0.0.1", serverSocketPort);
 
                 CompletableFuture<Socket> socketFuture = new CompletableFuture<>();
                 new Scheduler.SchedulerThreadFactory("MockServer Test " + this.getClass().getSimpleName()).newThread(() -> {
@@ -91,38 +98,23 @@ public class BinaryProxyIntegrationTest {
                     .setThrowable(se)
             );
             throw se;
+        } finally {
+            if (proxyClientAndServer != null) {
+                proxyClientAndServer.stop();
+            }
         }
     }
 
-    // TODO(jamesdbloom) make this test more stable:
-    //      org.mockserver.client.SocketConnectionException: Channel handler removed before valid response has been received
-    //        at org.mockserver.client.HttpClientConnectionErrorHandler.handlerRemoved(HttpClientConnectionErrorHandler.java:20)
-    //        at io.netty.channel.AbstractChannelHandlerContext.callHandlerRemoved(AbstractChannelHandlerContext.java:946)
-    //        at io.netty.channel.DefaultChannelPipeline.callHandlerRemoved0(DefaultChannelPipeline.java:637)
-    //        at io.netty.channel.DefaultChannelPipeline.destroyDown(DefaultChannelPipeline.java:876)
-    //        at io.netty.channel.DefaultChannelPipeline.destroyUp(DefaultChannelPipeline.java:844)
-    //        at io.netty.channel.DefaultChannelPipeline.destroy(DefaultChannelPipeline.java:836)
-    //        at io.netty.channel.DefaultChannelPipeline.access$700(DefaultChannelPipeline.java:46)
-    //        at io.netty.channel.DefaultChannelPipeline$HeadContext.channelUnregistered(DefaultChannelPipeline.java:1392)
-    //        at io.netty.channel.AbstractChannelHandlerContext.invokeChannelUnregistered(AbstractChannelHandlerContext.java:198)
-    //        at io.netty.channel.AbstractChannelHandlerContext.invokeChannelUnregistered(AbstractChannelHandlerContext.java:184)
-    //        at io.netty.channel.DefaultChannelPipeline.fireChannelUnregistered(DefaultChannelPipeline.java:821)
-    //        at io.netty.channel.AbstractChannel$AbstractUnsafe$8.run(AbstractChannel.java:826)
-    //        at io.netty.util.concurrent.AbstractEventExecutor.safeExecute(AbstractEventExecutor.java:164)
-    //        at io.netty.util.concurrent.SingleThreadEventExecutor.runAllTasks(SingleThreadEventExecutor.java:472)
-    //        at io.netty.channel.nio.NioEventLoop.run(NioEventLoop.java:497)
-    //        at io.netty.util.concurrent.SingleThreadEventExecutor$4.run(SingleThreadEventExecutor.java:989)
-    //        at io.netty.util.internal.ThreadExecutorMap$2.run(ThreadExecutorMap.java:74)
-    //        at java.lang.Thread.run(Thread.java:748)
     @Test
     public void shouldForwardBinaryMessagesOverTLS() throws Exception {
         // given
-        byte[] randomRequestBytes = RandomUtils.nextBytes(100);
-        byte[] randomResponseBytes = RandomUtils.nextBytes(100);
+        byte[] randomRequestBytes = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
+        byte[] randomResponseBytes = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
+        ClientAndServer proxyClientAndServer = null;
         try {
             try (SSLServerSocket serverSocket = sslSocketFactory().wrapSocket()) {
                 // and
-                ClientAndServer proxyClientAndServer = startClientAndServer("127.0.0.1", serverSocket.getLocalPort());
+                proxyClientAndServer = startClientAndServer("127.0.0.1", serverSocket.getLocalPort());
 
                 CompletableFuture<Socket> socketFuture = new CompletableFuture<>();
                 new Scheduler.SchedulerThreadFactory("MockServer Test " + this.getClass().getSimpleName()).newThread(() -> {
@@ -164,13 +156,17 @@ public class BinaryProxyIntegrationTest {
                     .setThrowable(se)
             );
             throw se;
+        } finally {
+            if (proxyClientAndServer != null) {
+                proxyClientAndServer.stop();
+            }
         }
     }
 
     @Test
     public void shouldCloseConnectionForBinaryMessagesWithNoRemoteAddress() throws Exception {
         // given
-        byte[] randomRequestBytes = RandomUtils.nextBytes(100);
+        byte[] randomRequestBytes = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
         ClientAndServer clientAndServer = startClientAndServer();
 
         // when
