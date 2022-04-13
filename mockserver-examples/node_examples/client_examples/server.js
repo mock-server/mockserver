8b89eeeabb67cc25e610a01a74c82e87e0f7bc7a
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2022-04-13 15:40:13 +0100

    improved documentation and examples for providing JVM options to mockserver-node

--- mockserver-examples/node_examples/client_examples/server.js
+++ mockserver-examples/node_examples/client_examples/server.js
@@ -2,7 +2,17 @@ function runMockServer() {
     var mockserver = require('mockserver-node');
 
     mockserver
-        .start_mockserver({serverPort: 1080, verbose: true})
+        .start_mockserver({
+            serverPort: 1080,
+            jvmOptions: [
+                '-Dmockserver.enableCORSForAllResponses=true',
+                '-Dmockserver.corsAllowMethods="CONNECT, DELETE, GET, HEAD, OPTIONS, POST, PUT, PATCH, TRACE"',
+                '-Dmockserver.corsAllowHeaders="Allow, Content-Encoding, Content-Length, Content-Type, ETag, Expires, Last-Modified, Location, Server, Vary, Authorization"',
+                '-Dmockserver.corsAllowCredentials=true -Dmockserver.corsMaxAgeInSeconds=300'
+            ],
+            mockServerVersion: "5.13.2",
+            verbose: true
+        })
         .then(
             function () {
                 console.log("started MockServer");
