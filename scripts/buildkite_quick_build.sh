3315eb72087a897f4795026aac4aeb9f5b89a0d3
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2023-02-18 10:29:38 +0000

    disabling while investigating weird memory issue suddenly appearing only in build server

--- scripts/buildkite_quick_build.sh
+++ scripts/buildkite_quick_build.sh
@@ -4,28 +4,28 @@ set -euo pipefail
 
 #export MAVEN_OPTS="$MAVEN_OPTS -Xms2048m -Xmx8192m"
 #export JAVA_OPTS="$JAVA_OPTS -Xms2048m -Xmx8192m"
-echo
-free -mh
-ulimit -u
-ulimit -a
-ps -eLf | grep 'myuser' | wc -l
-ps -eLf | wc -l
-sysctl kernel.pid_max
-sysctl -w kernel.pid_max=4194303
-sysctl kernel.pid_max
-echo
-java -version
-echo
-./mvnw -version
-echo
-
-if test "$BUILDKITE_BRANCH" = "master"; then
-    echo "BRANCH: MASTER"
-else
-    echo "BRANCH: $CURRENT_BRANCH"
-fi
-
-echo "whoami: "
-whoami
-
-./mvnw clean install $1
+#echo
+#free -mh
+#ulimit -u
+#ulimit -a
+#ps -eLf | grep 'myuser' | wc -l
+#ps -eLf | wc -l
+#sysctl kernel.pid_max
+#sysctl -w kernel.pid_max=4194303
+#sysctl kernel.pid_max
+#echo
+#java -version
+#echo
+#./mvnw -version
+#echo
+#
+#if test "$BUILDKITE_BRANCH" = "master"; then
+#    echo "BRANCH: MASTER"
+#else
+#    echo "BRANCH: $CURRENT_BRANCH"
+#fi
+#
+#echo "whoami: "
+#whoami
+#
+#./mvnw clean install $1
