8cdc2b47bd0c63326a276f7e47c4b169bd1f7d25
James D Bloom <733179+jamesdbloom@users.noreply.github.com>
2026-05-02 13:55:09 +0100

    updated local_quick_build.sh to use java 17

--- scripts/local_quick_build.sh
+++ scripts/local_quick_build.sh
@@ -4,7 +4,7 @@ set -e
 
 export MAVEN_OPTS="$MAVEN_OPTS -Xms8192m -Xmx8192m"
 export JAVA_OPTS="$JAVA_OPTS -Xms8192m -Xmx8192m"
-export JAVA_HOME=`/usr/libexec/java_home -v 13`
+export JAVA_HOME=`/usr/libexec/java_home -v 17`
 echo
 java -version
 echo
@@ -12,6 +12,6 @@ echo
 echo
 
 # to run from specific module use argument in quotes "-rf mockserver-war"
-./mvnw -T 3C clean install -offline $1 -Djava.security.egd=file:/dev/urandom
+./mvnw -T 3C clean install ${1:-} -Djava.security.egd=file:/dev/urandom
 
 SKIP_JAVA_BUILD=true container_integration_tests/integration_tests.sh
